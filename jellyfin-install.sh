c0ccaf036c8dce355a590ffb918b18aac2c391d6
excelle1978 <114529895+excelle1978@users.noreply.github.com>
2022-09-28 15:22:00 -0500

    Update jellyfin-install.sh

--- jellyfin-install.sh
+++ jellyfin-install.sh
@@ -1,5 +1,5 @@
 sudo apt update && sudo apt upgrade -y
-sudo apt install apt-transport-https ca-certificates gnupg2 curl git -y
+sudo apt install apt-transport-https ca-certificates gnupg2 libvpx6 libwebp6 libx264-160 libx265-192 curl git -y
 sudo wget -O- https://repo.jellyfin.org/jellyfin_team.gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/jellyfin.gpg
 echo "deb [arch=$( dpkg --print-architecture ) signed-by=/usr/share/keyrings/jellyfin.gpg] https://repo.jellyfin.org/debian bullseye main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
 sudo apt update
